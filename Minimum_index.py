838690d74cc21c6fcb26d8b2bf70f9e16051bf9c
Jide <52969708+Faleye-jide@users.noreply.github.com>
2021-04-27 01:09:31 +0100

    Add files via upload

---
+++ Minimum_index.py
@@ -0,0 +1,33 @@
+def indexOfMin(list):
+    minIndex = 0
+    currentIndex = 1
+    n = len(list)
+
+    while currentIndex < n:
+        if list[currentIndex] < list[minIndex]:
+            minIndex = currentIndex
+        else:
+            currentIndex += 1
+
+    return minIndex
+
+num = [2,3,4,1,7,9]
+
+print(indexOfMin(num))
+
+
+def indexMin(list):
+    minIndex = list[0]
+    n = len(list)
+
+
+    for i in range(1, n):
+        if list[i] < list[minIndex]:
+            minIndex = list[i]
+        i += 1
+
+    return minIndex
+
+num = [2,3,4,1,7,9]
+
+print(indexOfMin(num))

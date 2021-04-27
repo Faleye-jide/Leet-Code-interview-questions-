257f22e3cf99da0e6906c5ad789b79b4f488b731
Jide <52969708+Faleye-jide@users.noreply.github.com>
2021-04-27 01:03:20 +0100

    Update Find_duplicates.py

--- Find_duplicates.py
+++ Find_duplicates.py
@@ -1,15 +1,15 @@
-# def duplicates(arr):
-#     if arr is None:
-#         return
-#
-#     for i in range(0,len(arr)-1):
-#         if arr[i] == arr[i+1]:
-#             return True
-#         i+=1
-#
-#     return False
-# number = [1,1,3,4,5,6,7]
-# print(duplicates(number))
+def duplicates(arr):
+    if arr is None:
+        return
+
+    for i in range(0,len(arr)-1):
+        if arr[i] == arr[i+1]:
+            return True
+        i+=1
+
+    return False
+number = [1,1,3,4,5,6,7]
+print(duplicates(number))
 
 # set approach
 def duplicates(arr):

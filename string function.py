30a69932fbd5c15ace66a5b64fb10452b0063050
Jide <52969708+Faleye-jide@users.noreply.github.com>
2021-07-18 16:40:45 +0100

    Add files via upload

---
+++ string function.py
@@ -0,0 +1,18 @@
+s = "Hello world"
+# def str(string):
+#     length = 0
+#     for i in range(len(s)):
+#         if s[i] == " ":
+#             length = 0
+#         else:
+#             length+=1
+#
+#     return length
+# print(str(string=s))
+
+
+# another solution
+def str(string):
+    return len(s.split()[-1])
+print(s.split())
+print(str(string=s))
